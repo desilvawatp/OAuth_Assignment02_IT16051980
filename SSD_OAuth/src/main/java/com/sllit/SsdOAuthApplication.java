c25b216d4f68f8021a454284c04f6117a1501689
Admin <desilva.watp@gmail.com git config --global user.name desilvawatp>
2019-09-25 00:51:40 +0530

    Final commit 2

---
+++ SSD_OAuth/src/main/java/com/sllit/SsdOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.sllit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsdOAuthApplication.class, args);
+	}
+
+}
